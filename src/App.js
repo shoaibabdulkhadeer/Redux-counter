03bf19c9e2f4e43f7ac6f03e9c26bfd17e8a4b93
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-05-07 14:24:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { increment } from './actions';
+import { decrement } from './actions';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+
 
 function App() {
+
+  const counter = useSelector(state => state.counter)
+const dispath = useDispatch()
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{counter}</p>
+      <button onClick={() => dispath(increment())}>+</button>
+      <button onClick={() => dispath(decrement())}>-</button>
     </div>
   );
 }
